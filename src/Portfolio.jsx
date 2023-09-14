097f4750ad4c1c46c03570e971a1ba50d308e892
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-09-14 15:25:46 -0700

    feat: implement TailwindCSS; create JSON for color themes.

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -2,127 +2,28 @@ import './Portfolio.css';
 
 export default function Portfolio() {
 	return (
-		<>
-			<div id="color-theme_titan">
-				<h1>
-					Combining <span className="art-font">Arts</span> and{' '}
-					<span className="code-font">Development</span>
-				</h1>
-				<div className="secondary-color-block">
-					<h2>list of things:</h2>
-					<ul>
-						<li>this</li>
-						<li>that</li>
-						<li>here</li>
-						<li>there</li>
-					</ul>
-				</div>
-				<div className="tertiary-color-block">
-					<h2>Some stuff idk</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-						a, ratione tempore ipsa atque minima veritatis aut mollitia animi
-						eaque, voluptatem odit cupiditate vero ducimus? Modi architecto ut
-						amet ab.
-					</p>
-				</div>
+		<div id="color-theme_titan" className="dark:bg-slate-800 text-red-100">
+			<h1 className="text-4xl">
+				Combining <span className="art-font">Arts</span> and{' '}
+				<span className="code-font">Development</span>
+			</h1>
+			<div className="">
+				<h2>list of things:</h2>
+				<ul>
+					<li>this</li>
+					<li>that</li>
+					<li>here</li>
+					<li>there</li>
+				</ul>
 			</div>
-			<div id="color-theme_ionic">
-				<h1>
-					Combining <span className="art-font">Arts</span> and{' '}
-					<span className="code-font">Development</span>
-				</h1>
-				<div className="secondary-color-block">
-					<h2>list of things:</h2>
-					<ul>
-						<li>this</li>
-						<li>that</li>
-						<li>here</li>
-						<li>there</li>
-					</ul>
-				</div>
-				<div className="tertiary-color-block">
-					<h2>Some stuff idk</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-						a, ratione tempore ipsa atque minima veritatis aut mollitia animi
-						eaque, voluptatem odit cupiditate vero ducimus? Modi architecto ut
-						amet ab.
-					</p>
-				</div>
+			<div className="">
+				<h2>Some stuff idk</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, a,
+					ratione tempore ipsa atque minima veritatis aut mollitia animi eaque,
+					voluptatem odit cupiditate vero ducimus? Modi architecto ut amet ab.
+				</p>
 			</div>
-			<div id="color-theme_reef">
-				<h1>
-					Combining <span className="art-font">Arts</span> and{' '}
-					<span className="code-font">Development</span>
-				</h1>
-				<div className="secondary-color-block">
-					<h2>list of things:</h2>
-					<ul>
-						<li>this</li>
-						<li>that</li>
-						<li>here</li>
-						<li>there</li>
-					</ul>
-				</div>
-				<div className="tertiary-color-block">
-					<h2>Some stuff idk</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-						a, ratione tempore ipsa atque minima veritatis aut mollitia animi
-						eaque, voluptatem odit cupiditate vero ducimus? Modi architecto ut
-						amet ab.
-					</p>
-				</div>
-			</div>
-			<div id="color-theme_blossom">
-				<h1>
-					Combining <span className="art-font">Arts</span> and{' '}
-					<span className="code-font">Development</span>
-				</h1>
-				<div className="secondary-color-block">
-					<h2>list of things:</h2>
-					<ul>
-						<li>this</li>
-						<li>that</li>
-						<li>here</li>
-						<li>there</li>
-					</ul>
-				</div>
-				<div className="tertiary-color-block">
-					<h2>Some stuff idk</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-						a, ratione tempore ipsa atque minima veritatis aut mollitia animi
-						eaque, voluptatem odit cupiditate vero ducimus? Modi architecto ut
-						amet ab.
-					</p>
-				</div>
-			</div>
-			<div id="color-theme_cacao">
-				<h1>
-					Combining <span className="art-font">Arts</span> and{' '}
-					<span className="code-font">Development</span>
-				</h1>
-				<div className="secondary-color-block">
-					<h2>list of things:</h2>
-					<ul>
-						<li>this</li>
-						<li>that</li>
-						<li>here</li>
-						<li>there</li>
-					</ul>
-				</div>
-				<div className="tertiary-color-block">
-					<h2>Some stuff idk</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-						a, ratione tempore ipsa atque minima veritatis aut mollitia animi
-						eaque, voluptatem odit cupiditate vero ducimus? Modi architecto ut
-						amet ab.
-					</p>
-				</div>
-			</div>
-		</>
+		</div>
 	);
 }
